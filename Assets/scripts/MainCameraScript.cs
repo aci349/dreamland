c66e8950cf68ea8130203bb833a1835b93b4e81d
TheMajonese <40393093+TheMajonese@users.noreply.github.com>
2018-06-24 17:28:43 +0200

    Add files via upload

---
+++ Assets/scripts/MainCameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour {
+
+	private GameObject player;			//reference to the player gameobject
+
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+
+		transform.position = player.transform.position;
+		transform.rotation = player.transform.rotation;
+	}
+	
+	void Update ()
+	{
+		float h = 3 * Input.GetAxis("Mouse X");
+		float v = -3 * Input.GetAxis("Mouse Y");
+		transform.Rotate(v, h, 0);
+
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+	}
+}
---
+++ Assets/scripts/Player/PlayerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	[SerializeField]
+	private float movementSpeed;                //vertical Player movement speed
+
+	private Rigidbody entityRB;               //reference to the player's Rigidbody
+	private Transform groundCheck;              //reference to the ground check's transform
+
+	private bool grounded;                      //bool to check if the player is on viable ground
+
+	void Start()
+	{
+		entityRB = GetComponent<Rigidbody>();                 //initialize player Rigidbody
+		groundCheck = transform.Find("playerGroundCheck");      //initialize groundCheck Transform
+	}
+	
+	void Update ()
+	{
+		//grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		//if (grounded)
+		//{
+		
+		if (Input.GetKey("a"))
+		{
+			entityRB.velocity = new Vector3(movementSpeed * -1, entityRB.velocity.y, entityRB.velocity.z);
+		}
+		else if (Input.GetKey("d"))
+		{
+			entityRB.velocity = new Vector3(movementSpeed, entityRB.velocity.y, entityRB.velocity.z);
+		}
+
+		if (Input.GetKey("w"))
+		{
+			entityRB.velocity = new Vector3(entityRB.velocity.x, entityRB.velocity.y, movementSpeed);
+		}
+		else if (Input.GetKey("s"))
+		{
+			entityRB.velocity = new Vector3(entityRB.velocity.x, entityRB.velocity.y, movementSpeed * -1);
+		}
+	}
+}
