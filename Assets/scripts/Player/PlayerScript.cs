04ef94bb7c353123604489d80f9a962a4d1e7b51
TheMajonese <40393093+TheMajonese@users.noreply.github.com>
2018-06-27 12:07:04 +0200

    Add files via upload

--- Assets/scripts/Player/PlayerScript.cs
+++ Assets/scripts/Player/PlayerScript.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
-	[SerializeField]
-	private float movementSpeed;                //vertical Player movement speed
+    [SerializeField]
+    private float movementSpeed;                //vertical Player movement speed
 
-	private Rigidbody entityRB;               //reference to the player's Rigidbody
-	private Transform groundCheck;              //reference to the ground check's transform
+    private Rigidbody entityRB;               //reference to the player's Rigidbody
+    private Transform groundCheck;              //reference to the ground check's transform
 
-	private bool grounded;                      //bool to check if the player is on viable ground
+    private bool grounded;                      //bool to check if the player is on viable ground
+    private bool floating;                      //bool to check if the player is floating
 
-	void Start()
+    public bool Grounded
+    {
+        get
+        {
+            return grounded;
+        }
+        set
+        {
+            grounded = value;
+        }
+    }
+    public bool Floating
+    {
+        get
+        {
+            return floating;
+        }
+        set
+        {
+            floating = value;
+        }
+    }
+    public Rigidbody EntityRB
+    {
+        get
+        {
+            return entityRB;
+        }
+        set
+        {
+            entityRB = value;
+        }
+    }
+
+    void Start()
 	{
 		entityRB = GetComponent<Rigidbody>();                 //initialize player Rigidbody
 		groundCheck = transform.Find("playerGroundCheck");      //initialize groundCheck Transform
@@ -20,7 +55,7 @@ public class PlayerScript : MonoBehaviour {
 	
 	void Update ()
 	{
-		//grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        //grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
 		//if (grounded)
 		//{
@@ -42,5 +77,22 @@ public class PlayerScript : MonoBehaviour {
 		{
 			entityRB.velocity = new Vector3(entityRB.velocity.x, entityRB.velocity.y, movementSpeed * -1);
 		}
-	}
+
+        if (floating)
+        {
+            if (Input.GetKey("space"))
+            {
+                entityRB.velocity = new Vector3(entityRB.velocity.x, movementSpeed, entityRB.velocity.z);
+            }
+            else if (Input.GetKey("left shift"))
+            {
+                entityRB.velocity = new Vector3(entityRB.velocity.x, -movementSpeed, entityRB.velocity.z);
+            }
+        }
+    }
+
+    public Rigidbody ReturnRigidbody()
+    {
+        return entityRB;
+    }
 }
