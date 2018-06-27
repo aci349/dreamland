dd4943be3115b131b43f27373ac1b9fb0ebb9ecf
aci349 <40387763+aci349@users.noreply.github.com>
2018-06-27 10:53:08 +0200

    mentos

---
+++ Assets/scripts/mentos.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mentos : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject mentos = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        mentos.transform.localScale -= new Vector3(0.5f, 0.8f, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
