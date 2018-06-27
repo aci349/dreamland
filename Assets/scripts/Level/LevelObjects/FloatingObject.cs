22869c53d030a59a6bc9e79c0892248aa2260eb4
TheMajonese <40393093+TheMajonese@users.noreply.github.com>
2018-06-27 12:07:25 +0200

    Add files via upload

---
+++ Assets/scripts/Level/LevelObjects/FloatingObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObject : MonoBehaviour {
+
+    private Rigidbody playerRB;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            if(!playerRB)
+                playerRB = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>().EntityRB;
+
+            playerRB.useGravity = false;
+            playerRB.velocity = new Vector3(playerRB.velocity.x, 0, playerRB.velocity.z);
+            col.GetComponent<PlayerScript>().Floating = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            playerRB.useGravity = true;
+            col.GetComponent<PlayerScript>().Floating = false;
+        }
+    }
+}
