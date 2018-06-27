87bb54af29c5a3cdcc7f73e53de28d97f8ba12c5
PaoloArcheron <40387887+PaoloArcheron@users.noreply.github.com>
2018-06-27 12:04:47 +0200

    Door and Key (Noch nicht fertig)

---
+++ Assets/scripts/Door and Key/DoorKey.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorKey : MonoBehaviour
+{
+
+    public bool inTrigger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DoorScript.doorKey = true;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (inTrigger)
+        {
+            GUI.Box(new Rect(0, 60, 200, 25), "Press E to take key");
+        }
+    }
+}
