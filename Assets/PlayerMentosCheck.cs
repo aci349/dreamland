abb8dfcbdc86407b3874e0179d86b560e4b63db2
aci349 <40387763+aci349@users.noreply.github.com>
2018-07-04 12:08:13 +0200

    Add files via upload

---
+++ Assets/PlayerMentosCheck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMentosCheck : MonoBehaviour {
+
+    public mentos mentos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "mentos")
+        {
+            foreach(GameObject go in mentos.allementos)
+            {
+                if(other == go)
+                {
+                    go.transform.Rotate(360, 0, 0);
+                    if(go == mentos.mentoone)
+                    {
+                        mentos.mentoonehit = true;
+                    } else if (go == mentos.mentotwo)
+                    {
+                        mentos.mentotwohit = true;
+                    } else
+                    {
+                        mentos.mentothreehit = true;
+                    }
+                }
+            }
+        }
+    }
+
+}
