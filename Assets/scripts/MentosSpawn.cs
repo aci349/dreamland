7b6f0bf14209e851268f763ae5c07f89c37f38cd
aci349 <40387763+aci349@users.noreply.github.com>
2018-07-07 14:47:22 +0200

    Add files via upload

---
+++ Assets/scripts/MentosSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MentosSpawn : MonoBehaviour {
+    
+    public GameObject einMentos;
+    public int cntMentos;
+    public float timer;
+    private float spawntimer = 3f;
+
+    // Use this for initialization
+    void Start () {
+
+        timer = 0;
+        cntMentos = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+        if (timer >= spawntimer)
+        {
+            if (cntMentos < 3)
+            {
+                Instantiate(einMentos);
+                cntMentos++;
+
+                timer = 0;
+            }
+        }
+    }
+
+    public void ChangeCnt ()
+    {
+        cntMentos--;
+    }
+}
---
+++ Assets/scripts/PlayerMentosCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMentosCheck : MonoBehaviour
+{
+    public GameObject spawner;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "mentos")
+        {
+            other.GetComponent<Mentos>().MentosHit();
+
+            spawner.GetComponent<MentosSpawn>().ChangeCnt();
+        }
+    }
+
+}
