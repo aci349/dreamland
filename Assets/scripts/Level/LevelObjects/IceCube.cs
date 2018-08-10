ff9b3fe82f435d1db8bc6b3040fc1f6a3f81d6b5
TheMajonese <40393093+TheMajonese@users.noreply.github.com>
2018-08-10 12:53:03 +0200

    IceCube Script (no pickup)

---
+++ Assets/scripts/Level/LevelObjects/IceCube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCube : MonoBehaviour {
+
+    private GameObject player;
+    private bool tracking;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        tracking = true;
+    }
+
+
+    void Update()
+    {
+        if (tracking)
+        {
+            Vector3 target = player.transform.position;
+
+            transform.position = Vector3.Lerp(transform.position, player.transform.position, 0.01f);
+
+        }
+
+    }
+}
