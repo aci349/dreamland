75dac6b85a6aa0f911a12bb4307767cbb07b9ce1
PaoloArcheron <40387887+PaoloArcheron@users.noreply.github.com>
2018-09-20 16:05:19 +0200

    Add files via upload
    Script für die letze Tür und Zielscheibe. Wenn die Zielscheibe 3 mal getroffen wird die Tür offnet sich.

---
+++ Assets/scripts/LastDoorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+	
+public class LastDoorScript : MonoBehaviour {
+	
+	public bool isTriggered;
+    public int door1;
+	
+	//tür wartet bis die Zielscheibe 3 mal getroffen wir
+    public void Door ()
+
+    {
+        door1++;
+
+        if (door1 == 3)
+            OpenDoor();
+    }
+
+	//tür ist weg
+ void OpenDoor()
+ 
+    {
+        Destroy(this.gameObject);
+
+    }
+}
---
+++ Assets/scripts/Target.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Target : MonoBehaviour {
+	
+	public bool isTriggered;
+
+	//durch ein box collider wird das werfen präziser
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "werfen")
+		{	
+			 Destroy(other.gameObject);
+			 isTriggered = true;
+            GameObject.Find("Room1_Gate").GetComponent<LastDoorScript>().Door();
+
+			Destroy(this.gameObject);
+			
+        
+		}
+	}
+	//die tür offnet sich
+	//bonbon ist dann weg
+	
+	void Update () {
+		
+		}
+		
+}
+
+		
+		
