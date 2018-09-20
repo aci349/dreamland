fa3b745b675c147926826270d38ccd4c793ac973
aci349 <40387763+aci349@users.noreply.github.com>
2018-09-20 13:46:36 +0200

    changes to mentos and bubbles
    > bubbles spawn where mentos hits
    > mentos now has a box collider to match with pickup script
    > mentos falls over with new collider

---
+++ Assets/scripts/Bubble.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour {
+	
+    float currsize;
+    float maxsize;
+    bool downsize;
+
+	// Use this for initialization
+	void Start () {
+        downsize = false;
+        currsize = 0.01f;
+        maxsize = 1.1f;
+
+        float posx = Random.Range(-1.5f, 1.5f);
+        float posz = Random.Range(-1.5f, 1.5f);
+        transform.position = new Vector3(transform.position.x + posx, transform.position.y - 0.25f, transform.position.z + posz);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (downsize == false)
+        {
+            currsize += 0.02f;
+            transform.localScale = new Vector3(currsize, currsize, currsize);
+            if (currsize >= maxsize)
+            {
+                downsize = true;
+            }
+        } else
+        {
+            currsize -= currsize;
+            transform.localScale = new Vector3(currsize, currsize, currsize);
+            if (currsize <= 0.01f)
+            {
+                float posx = Random.Range(-1.5f, 1.5f);
+                float posz = Random.Range(-1.5f, 1.5f);
+                transform.position = new Vector3(transform.position.x + posx, transform.position.y, transform.position.z + posz);
+                downsize = false;
+            }
+        }
+	}
+}
---
+++ Assets/scripts/Pfuetze.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pfuetze : MonoBehaviour {
+
+	[SerializeField]
+    private bool mentoshit;
+    private float platformtimerup = 3f;
+    public float timer;
+    public GameObject einBubble;
+	private Transform spawnposition;
+
+    //ontriggerenter 
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "mentos")
+        {
+			spawnposition = collision.gameObject.transform;
+            mentoshit = true;
+            Bubbles();
+        }
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.tag == "Player" && mentoshit == true)
+        {
+            if (timer >= platformtimerup)
+            {
+                collision.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 1500f, 0f));
+                timer = 0;
+            }
+        }
+    }
+
+    private void Bubbles()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            Instantiate(einBubble, spawnposition.position, spawnposition.rotation);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        timer = 0;
+        mentoshit = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (mentoshit == true)
+        {
+            timer += Time.deltaTime;
+        }
+    }
+}
