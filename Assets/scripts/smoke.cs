1a1c38652bacfc2cbde0a802d8067a3fd3163b2e
sabubble <40388062+sabubble@users.noreply.github.com>
2018-09-21 21:44:13 +0200

    Rauch als Deko für Medizinwelt
    kommt aus Joint

--- Assets/scripts/smoke.cs
+++ Assets/scripts/smoke.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class smoke : MonoBehaviour {
 
 	private GameObject joint;
-	private GameObject smokeSphere;
 	
 	public float timeStart = 0f;
 	public float timeStop;
@@ -15,38 +14,32 @@ public class smoke : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		joint = GameObject.Find("Cylinder");
-		smokeSphere = GameObject.Find("Sphere");
 		joint.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-		smokeSphere.gameObject.GetComponent<Rigidbody>().isKinematic = true;
 		
-		smokeSphere.transform.position = joint.transform.position;
-		timeStop = Random.Range(5f, 13f);	
+		transform.position = joint.transform.position;
+		timeStop = Random.Range(1f, 4f);	
 	} 
 	
 	// Update is called once per frame
 	void Update () {
 		producesmoke();
 		if(stop == true){
-			Debug.Log("TestObSmokeSphereNullIst");
 			timeStart = 0f;
 			producesmoke();
 		}
 	}                                   
 	
 	void producesmoke(){
-		Debug.Log("TestTimerStart");
 		if(timeStart < timeStop){	
 			timeStart += Time.deltaTime;
 			stop = false;
-			smokeSphere.transform.Translate(Vector3.up * Time.deltaTime * Random.Range(0.5f, 3f), Space.World);	//Rauch/Spehre steigt nach oben
-			Debug.Log("TestSphereVergrößern");
-			smokeSphere.transform.localScale += new Vector3(Random.Range(0.005f, 0.03f), Random.Range(0.005f, 0.03f), Random.Range(0.005f, 0.03f));	//Rauch/Sphere wird größer
+			transform.Translate(Vector3.up * Time.deltaTime * Random.Range(0.5f, 3f), Space.World);	//Rauch steigt nach oben
+			transform.localScale += new Vector3(Random.Range(0.005f, 0.03f), Random.Range(0.005f, 0.03f), Random.Range(0.005f, 0.03f));	//Rauch wird größer
 		}
 		else{
-			Debug.Log("TestTimerStop");
 			stop = true;
-			smokeSphere.transform.position = joint.transform.position;
-			smokeSphere.transform.localScale = new Vector3(sphereSize, sphereSize, sphereSize);
+			transform.position = joint.transform.position;	//Rauch wird zurück gesetzt
+			transform.localScale = new Vector3(sphereSize, sphereSize, sphereSize);
 		}
 	}
 	
